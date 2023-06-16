ea7fe2ae729bd2cdbacc84d5308e1375bba469e0
BmDevelopers001 <bmsavaliya001@gmail.com>
2023-06-16 18:01:33 +0530

    Shopping cart

---
+++ Shopping-cart/src/App.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+
+import Item from "./Items/Item"
+
+import { Drawer, LinearProgress, Grid, Badge } from "@mui/material";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+
+import { Wrapper, StyledButton } from "./App.Style";
+
+export type CartItemType = {
+  id: number,
+  category: string,
+  description: string,
+  image: string,
+  price: number,
+  title: string,
+  amount: number
+}
+
+const getProducts = async (): Promise<CartItemType[]> => 
+      await (await fetch('https://fakestoreapi.com/products')).json();
+
+ function App() {
+  const [cartOpen, setCartOpen] = useState(false);
+  const[cartItems, setCartItems] = useState([] as CartItemType[]);
+  const {data,isLoading,error} = useQuery<CartItemType[]>('products', getProducts);
+  
+  const getTotalItems = (items : CartItemType[]) => {
+    return items.reduce((ack : number, item) => ack + item.amount, 0)
+  };
+
+  const handleAddToCart = (clickedItem : CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if(isLoading){
+    return <LinearProgress />
+  }
+  if(error){
+    return <div>Soemthing Went Wrong...</div>
+  }
+  
+  return (
+    <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart goes here
+      </Drawer>
+
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color= 'error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
+
+      <h1>Shopping Cart</h1>
+      <Grid container spacing={3}>
+        {data?.map( (item)=>{
+          return (
+            <Grid item key={item.id} xs={12} sm={4}>
+              <Item item={item} handleAddToCart={handleAddToCart} />
+            </Grid>
+          )
+        })}
+      </Grid>
+    </Wrapper>
+  )
+}
+
+export default App

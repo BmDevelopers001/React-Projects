799f52cd9e5ca9fa7cb73de493c1ce9ab3fc5f38
BmDevelopers001 <bmsavaliya001@gmail.com>
2023-06-19 12:35:56 +0530

    Shopping Cart Completed

--- Shopping-cart/src/App.tsx
+++ Shopping-cart/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
-import axios from "axios";
+// import axios from "axios";
 
 import Item from "./Items/Item"
+import Cart from "./Cart/Cart";
 
 import { Drawer, LinearProgress, Grid, Badge } from "@mui/material";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
@@ -31,9 +32,41 @@ const getProducts = async (): Promise<CartItemType[]> =>
     return items.reduce((ack : number, item) => ack + item.amount, 0)
   };
 
-  const handleAddToCart = (clickedItem : CartItemType) => null;
+  const handleAddToCart = (clickedItem : CartItemType) => {
+    setCartItems((prev) => {
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+      if(isItemInCart){
+        return prev.map((item) => {
+          if(item.id === clickedItem.id){
+            return {...item, amount: item.amount + 1};
+          } else {
+            return item;
+          }
+          
+        });
+      }
 
-  const handleRemoveFromCart = () => null;
+      return [...prev, {...clickedItem, amount : 1}];
+
+    })
+  };
+
+  const handleRemoveFromCart = (id:number) => {
+    setCartItems((prev) => {
+      return (
+        prev.reduce((ack, item) => {
+          if (item.id === id) {
+            if (item.amount === 1) {
+              return ack;
+            }
+            return [...ack, { ...item, amount: item.amount - 1 }];
+          } else {
+            return [...ack,item];
+          }
+        }, [] as CartItemType[])
+      )
+    })
+  };
 
   if(isLoading){
     return <LinearProgress />
@@ -45,7 +78,7 @@ const getProducts = async (): Promise<CartItemType[]> =>
   return (
     <Wrapper>
       <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
-        Cart goes here
+        <Cart cartItems={cartItems} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart}/>
       </Drawer>
 
       <StyledButton onClick={() => setCartOpen(true)}>
